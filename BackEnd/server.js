3d47103313903a24a1f373231c142972b25f6123
Jide Olawoye <g00364685@gmit.ie>
2020-11-16 00:05:19 +0000

    first commit

---
+++ BackEnd/server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+const port = 4000
+const cors = require('cors');
+
+const cors = require('cors');
+app.use(cors());
+app.use(function(req, res, next) {
+res.header("Access-Control-Allow-Origin", "*");
+res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+res.header("Access-Control-Allow-Headers",
+"Origin, X-Requested-With, Content-Type, Accept");
+next();
+});
+
+app.get('/api/movies', (req,res)=>{
+   
+    const myMovies = [
+        {
+            "Title": "Avengers: Infinity War",
+            "Year": "2018",
+            "imdbID": "tt4154756",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Captain America: Civil War",
+            "Year": "2016",
+            "imdbID": "tt3498820",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
+        }
+    ]
+    res.status(200).json({
+        message: 'Hello',
+        movies: myMovies});
+
+})
+
+app.post('/api/movies', (res, res)=>{
+    console.log('MOvies recieve!')
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
--- src/components/read.js
+++ src/components/read.js
@@ -1,37 +1,24 @@
 import React from 'react';
 import { Movies } from './movies';
+import {a} from
 
 
 export class Read extends React.Component {
 
     //Obj array filled with movies details
     state = {
-        movies:
-            [
-                {
-                    "Title": "Avengers: Infinity War",
-                    "Year": "2018",
-                    "imdbID": "tt4154756",
-                    "Type": "movie",
-                    "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-                },
-                {
-                    "Title": "Captain America: Civil War",
-                    "Year": "2016",
-                    "imdbID": "tt3498820",
-                    "Type": "movie",
-                    "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-                },
-                {
-                    "Title": "Charlie Wilson's War",
-                    "Year": "2007",
-                    "imdbID": "tt0472062",
-                    "Type": "movie",
-                    "Poster": "https://m.media-amazon.com/images/M/MV5BMTgwMDgwMDc4MF5BMl5BanBnXkFtZTYwOTU3MDM4._V1_SX300.jpg"
-                }
-            ]
-
+        movies:[]
     };
+
+    componentDidMount(){
+        axios.get('localhost:4000/api/movies')
+        .then((respomse)=>{
+            this.setState({movies: Response.data.Search})
+        })
+        .catch((error)=> {
+
+        })
+    }
 
     render() {
         return (
